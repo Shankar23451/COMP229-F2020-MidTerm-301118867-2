1570b8c3bb8a56ad2f14e09aa78ddff8224f3349
Shankar23451 <uramasam@my.centennialcollege.ca>
2020-11-10 08:03:31 -0500

    First commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://juj:jujfordinner@cluster0.ijym5.mongodb.net/Cluster0?retryWrites=true&w=majority"
+  //"mongodb://localhost/books229"
+};
